748a51acc815ae4f8efe898d226e8390b1edb016
undersourcecode <undersourcecode@hotmail.co.jp>
2018-11-22 07:32:50 +0900

    first commit.

---
+++ helloworld/README.md
@@ -0,0 +1,9 @@
+# Tensorflow helloworld
+
+## About This
+
+Tensorflowのhelloworld。
+
+https://www.tensorflow.org/tutorials/keras/basic_classification
+
+公式チュートリアルの最初のソースをやってみたもの。
---
+++ helloworld/main.py
@@ -0,0 +1,13 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    print(tf.__version__)
+
+if __name__ == "__main__":
+    main()
