58a43bac7d016be29caab303d9bee30f7dda2282
undersourcecode <undersourcecode@hotmail.co.jp>
2018-11-24 18:53:19 +0900

    画像をロード。

--- helloworld/main.py
+++ helloworld/main.py
@@ -9,5 +9,11 @@ import matplotlib.pyplot as plt
 def main():
     print("Hello, TensorFlow {}!".format(tf.__version__))
 
+    fashion_mnist = keras.datasets.fashion_mnist
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+    print("load_data finish!!")
+
+
 if __name__ == "__main__":
     main()
