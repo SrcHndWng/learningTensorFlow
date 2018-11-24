fc1b1d62516e49d6940c08fa553eebe2e92a32b2
undersourcecode <undersourcecode@hotmail.co.jp>
2018-11-25 08:06:21 +0900

    Preprocess the dataを実装。

--- helloworld/main.py
+++ helloworld/main.py
@@ -17,10 +17,17 @@ def main():
     class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-    plt.figure()
-    plt.imshow(train_images[0])
-    plt.colorbar()
-    plt.grid(False)
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
+
+    plt.figure(figsize=(10,10))
+    for i in range(25):
+        plt.subplot(5,5,i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(train_images[i], cmap=plt.cm.binary)
+        plt.xlabel(class_names[train_labels[i]])
     plt.show()
 
     print("----- finish!! -----")
